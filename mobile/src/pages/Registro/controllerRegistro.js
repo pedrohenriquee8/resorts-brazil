c77f83c4642dc820636389d2872160c39a27d28b
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2022-10-27 22:03:52 -0300

    Correção das variáveis

--- mobile/src/pages/Registro/controllerRegistro.js
+++ mobile/src/pages/Registro/controllerRegistro.js
@@ -1,11 +1,10 @@
-import SendingData from "../../services/sendingData";
+import SendingData from "../../services/registroService";
 
 export default class ControllerRegistro {
     registerUser = async (name, email, password) => {
         const sendingData = new SendingData();
         const dataToServer = await sendingData.registerUser(name, email, password);
         const status = dataToServer.status;
-        console.log("STATUS" + status);
         return status;
     };
 }
