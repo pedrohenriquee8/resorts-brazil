ba90a455ebdbc943cea8f0d951eee07d9b5aaa55
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2022-10-28 14:46:47 -0300

    Inserção da rota /login

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,31 @@ app.post("/register", async (req, res) => {
         console.log(error);
         return res.status(409).json({ sucess: false });
     }
-})
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            }
+        })
+        console.log(user);
+        if (user) {
+            if (password === user.password) {
+                return res.status(200).json({ sucess: true, user: user });
+            } else {
+                return res.status(409).json({ sucess: false });
+            }
+        } else {
+            return res.status(409).json({ sucess: false });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(409).json({ sucess: false });
+    }
+});
 
 app.listen(3000, () => {
     console.log("Servidor ouvindo na porta 3000");
